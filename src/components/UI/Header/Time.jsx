640405f0bc88e184b22f32ce3ede602586284586
Gibs <kwanzegs@gmail.com>
2023-02-26 18:26:42 +0700

    update header

--- src/components/UI/Header/Time.jsx
+++ src/components/UI/Header/Time.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import banner from "../../../../public/bannertwo.svg";
 
 const Time = () => {
-	return <div>Time</div>;
+	return (
+		<>
+			<div
+				className='container-fluid py-2'
+				style={{
+					backgroundImage: `url(${banner})`,
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+					backgroundRepeat: "no-repeat",
+					minHeight: "405px",
+				}}
+			></div>
+		</>
+	);
 };
 
 export default Time;
