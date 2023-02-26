e6bd758fa357175630747c6c39818ac0a8990f57
Gibs <kwanzegs@gmail.com>
2023-02-26 11:25:00 +0700

    update header

--- src/components/UI/Header/UserReview.jsx
+++ src/components/UI/Header/UserReview.jsx
@@ -1,62 +1,114 @@
-import React from "react";
-import userone from "../../../assets/userone.svg";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const UserReview = () => {
+	const [user, setUser] = useState([]);
+
+	useEffect(() => {
+		const getuser = async () => {
+			try {
+				const file = "../../../../public/data/UserReview.json";
+				const response = await axios.get(file);
+				const jsonData = response.data.data;
+				console.log(jsonData);
+				setUser(jsonData);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getuser();
+	}, []);
+
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		initialSlide: 0,
+		appendDots: (dots) => (
+			<div
+				style={{
+					padding: "3px",
+					height: "15px",
+				}}
+			>
+				<ul style={{ margin: "0px" }}> {dots} </ul>
+			</div>
+		),
+		rows: 2,
+
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+				},
+			},
+		],
+	};
+
 	return (
 		<>
-			<div className='container text-center'>
-				<h2
-					style={{
-						width: "310px",
-						marginLeft: "470px",
-						marginTop: "50px",
-						fontFamily: "Alegreya",
-					}}
-				>
-					О Симпозиуме
-				</h2>
-				<div className='row justify-content-center'>
-					<div className='col-4 just'>
-						<div
-							className='card mb-3 shadow-lg text-end justify-content-center'
-							style={{ width: "370px", height: "170px" }}
-						>
-							<div className='row g-0'>
-								<div className='col-md-4'>
-									<img
-										src={userone}
-										className=' mt-4'
-										alt=' '
-									/>
-								</div>
-								<div className='col-md-8'>
-									<div className='card-body'>
-										<h5 className='card-title text-start'>Cody Black</h5>
-										<p className='card-text  text-start'>
-											Aute ex laboris culpa dolor proident laborum exercitation
-											consequat culpa.
-										</p>
+			<div className='container text-center justify-content-center'>
+				<div className='row justify-content-center p-5'>
+					<h2
+						style={{
+							width: "310px",
+							marginTop: "5px",
+							fontFamily: "Alegreya",
+							fontWeight: "700",
+							fontSize: "48px",
+							lineHeight: "65.33px",
+							textAlign: "center",
+						}}
+					>
+						Докладчики
+					</h2>
+					<br />
+					<Slider {...settings}>
+						{user.map((item) => (
+							<div
+								className='card p-1 mb-3'
+								key={item.id}
+							>
+								<div className='row'>
+									<div className='col mb-3'>
+										<img
+											src={item.image}
+											className=' mt-4'
+											alt=' '
+										/>
+									</div>
+									<div className='col-md-8'>
+										<div className='card-body'>
+											<h5 className='card-title text-start'>
+												{item.first_name}&nbsp;{item.last_name}
+											</h5>
+											<p className='card-text  text-start'>{item.desc}</p>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					</div>
-
-					<div className='col-4'>
-						<span>USER</span>
-					</div>
-					<div className='col-4'>
-						<span>USER</span>
-					</div>
-					<div className='col-4'>
-						<span>USER</span>
-					</div>
-					<div className='col-4'>
-						<span>USER</span>
-					</div>
-					<div className='col-4'>
-						<span>USER</span>
-					</div>
+						))}
+					</Slider>
 				</div>
 			</div>
 		</>
